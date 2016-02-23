836d83cf3e897b9ba1728080bba75927e9ae9617
Sylvain Boily <sboily@proformatique.com>
2016-02-22 23:08:14 -0500

    missing coma

--- xivo.tf
+++ xivo.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "xivo" {
 
     provisioner "remote-exec" {
         inline = [
-            "wget --no-check-certificate https://raw.githubusercontent.com/sboily/xivo-aws/master/bin/xivo_install_aws -O /tmp/xivo_install_aws"
+            "wget --no-check-certificate https://raw.githubusercontent.com/sboily/xivo-aws/master/bin/xivo_install_aws -O /tmp/xivo_install_aws",
             "bash /tmp/xivo_install_aws"
         ]
 
