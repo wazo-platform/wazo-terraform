ce5fd8c6dda8a4db85a4cfb6be4ea2e8147c20db
Francois Blackburn <fblackburn@wazo.community>
2020-12-22 11:47:43 -0500

    Revert "update openstack image"
    This reverts commit bdb0b45977dd0efe627f7a607c4fbd9d039638c1.
    
    Older image has been found !!!

--- openstack/vars.tf
+++ openstack/vars.tf
@@ -35,7 +35,7 @@ variable "region" {
 
 variable "image_id" {
     description = "Openstack image id."
-    default = "ea1937cf-e38c-4218-8cf2-b5263593a0fe"
+    default = "b99574c3-33f5-4e3b-a5a6-e284cc0dcc4e"
 }
 
 variable "flavor_id" {
