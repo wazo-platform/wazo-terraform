5f84b0eeeb036cc965fa0f209649af1a455e8fd3
Marc Cesarine <marc.cesarine@wazo.io>
2025-10-10 11:20:57 -0400

    Fix AWS recipes

--- aws/vars.tf
+++ aws/vars.tf
@@ -1,83 +1,38 @@
-variable "access_key" {
-  description = "AWS access key."
-}
-
-variable "secret_key" {
-  description = "AWS secret key."
-}
-
 variable "region" {
   description = "The AWS region to create things in."
   default     = "us-east-1"
 }
 
-variable "key_name" {
-  description = "Name of the keypair to use in EC2."
-}
-
 variable "subnet_id" {
-  description = "Name of your subnet ID to use in EC2."
-}
-
-variable "private_key" {
-  description = "Path to your private key."
+  description = "ID of your subnet to use in EC2."
 }
 
 variable "instance_type" {
   description = "Instance type in AWS."
-  default     = "t2.micro"
+  default     = "t3a.medium"
 }
 
 variable "vpc_id" {
-  description = "VPC ID"
+  description = "ID of the VPC to use."
 }
 
-variable "amazon_amis" {
-  description = "Amazon Linux Debian AMIs"
-  default = {
-    us-east-1 = "ami-8b9a63e0"
-  }
+variable "amazon_ami_name_filter" {
+  description = "Filter to apply on names to retrieve AMI"
+  type        = string
+  default     = "debian-11*"
 }
 
-variable "count" {
-  default = 2
+variable "nb_instances" {
+  description = "Number of Wazo instances to create."
+  default     = 2
 }
 
-variable "user_name" {
-  description = "Openstack username."
+variable "public_key_path" {
+  description = "Path to ssh public key file to use to deploy instances."
+  type        = string
 }
 
-variable "password" {
-  description = "Openstack password."
-}
-
-variable "tenant_name" {
-  description = "Openstack Tenant name."
-}
-
-variable "auth_url" {
-  description = "Openstack keystone URL."
-}
-
-variable "region" {
-  description = "Openstack region name."
-  default     = "RegionOne"
-}
-
-variable "image_id" {
-  description = "Openstack image id."
-  default     = "b4e4e429-2b63-48fe-b885-855211cdf4e8"
-}
-
-variable "flavor_id" {
-  description = "Openstack flavor id."
-  default     = "2"
-}
-
-variable "key_pair" {
-  description = "Openstack key pair name."
-}
-
-variable "network" {
-  description = "Openstack network."
+variable "private_key_path" {
+  description = "Path to ssh private key file to use to deploy instances."
+  type        = string
 }
